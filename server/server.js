6e5eb1ef294a820085d889a47c0a8a191aa2720c
kimhyomin97 <rlagyalsdlek@naver.com>
2021-05-15 03:06:05 +0900

    remove routes

--- server/server.js
+++ server/server.js
@@ -13,10 +13,9 @@ const db = mysql.createPool({
 
 app.use(express.json());
 app.use(cors());
-// const api = require('./routes/index');
+
 // const PORT = process.env.PORT || 5000;
 const PORT = 5000;
-// const db = require('./config/db');
 
 app.get("/api/get", (req, res) => {
     const sqlQuery = "SELECT * FROM cocktail;";
@@ -25,24 +24,6 @@ app.get("/api/get", (req, res) => {
         console.log(result);
     })
 })
-
-// app.post("/api/insert", (req, res) => {
-//     const title = req.body.title;
-//     const content = req.body.content;
-//     const sqlQuery = "INSERT"...
-// })
-
-// app.use('/api', api);
-
-// app.get('/', (req, res) => {
-//     // db.query("SELECT * FROM cocktal", (err, data) => {
-//     //     if(!err) res.send({products : data});
-//     //     else res.send(err);
-//     // })
-
-//     // res.send('Server Response Success');
-//     res.send({ test : "test test"});
-// })
 
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
